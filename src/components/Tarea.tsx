f2df7da1610ebc6528fb446bc7f53ac059bc26eb
alanz3ky <ezequiel_matia@hotmail.com>
2024-08-17 21:23:46 +0200

    main changes

---
+++ src/components/Tarea.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface TaskProps {
+  task: string;
+  onDelete: () => void;
+}
+
+export const Task: React.FC<TaskProps> = ({ task, onDelete }) => {
+  return (
+    <div className='task'>
+      <span>{task}</span>
+      <button onClick={onDelete}>Eliminar</button>
+    </div>
+  );
+};
